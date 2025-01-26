bd670db05b692449334ca45553c1977c5c2457c8
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-26 23:05:13 +1300

    Refactor HTML code and add reference variables in JavaScript.
    - Refactored number button selectors in HTML to target each button individually.
    - Added reference variables in JavaScript to be used for event listeners.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Test Link");
+const displayText = document.querySelector(".display-text");
+const clearBtn = document.querySelector("#clear-btn")
+const percentButton = document.querySelector("#percent-btn")
+const backButton = document.querySelector("#back-btn")
+
+// Operator Reference
+const divisionButton = document.querySelector("#division-btn");
+const multiplyButton = document.querySelector("#multiply-btn");
+const subtractButton = document.querySelector("#subtract-btn");
+const additionButton = document.querySelector("#addition-btn");
+
+const equalButton = document.querySelector(".equal-btn");
+const decimalButton = document.querySelector(".decimal-btn");
+
+//Number Reference
+
+const zeroButton = document.querySelector("#zero-btn");
+const oneButton = document.querySelector("#one-btn");
+const twoButton = document.querySelector("#two-btn");
+const threeButton = document.querySelector("#three-btn");
+const fourButton = document.querySelector("#four-btn");
+const fiveButton = document.querySelector("#five-btn");
+const sixButton = document.querySelector("#six-btn");
+const sevenButton = document.querySelector("#seven-btn");
+const eightButton = document.querySelector("#eight-btn");
+const nineButton = document.querySelector("#nine-btn");

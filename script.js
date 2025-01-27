af36d09952c6635007cbe8307ac17271f83e9d3b
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-27 20:17:52 +1300

    Added new variables, functions and eventlisteners.
    - Variables to store the operands and operator of a Math operations.
    - Functions for basic Math operations.
    - Function  that invokes basic Math operations
    - Attached Eventlisteners to the number buttons variables.

--- script.js
+++ script.js
@@ -24,3 +24,78 @@ const sixButton = document.querySelector("#six-btn");
 const sevenButton = document.querySelector("#seven-btn");
 const eightButton = document.querySelector("#eight-btn");
 const nineButton = document.querySelector("#nine-btn");
+
+
+//Variables to store the operands and operator of a Math operations.
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+
+// Functions for basic Math operations.
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b; 
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if(b === 0) {
+        return "Cannot be divided by 0"
+    };
+
+    return a / b;
+}
+
+// Functions  that invokes basic Math operations 
+function operate(num1, num2, operator) {
+
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+     
+        case "−":
+            return subtract(num1, num2);
+     
+        case "×":
+            return multiply(num1, num2);
+     
+        case "÷":
+            return divide(num1, num2);
+        default:
+            return "Invalid Operator";
+    }
+}
+
+//Attached Eventlisteners to the number buttons variables.
+oneButton.addEventListener("click", () => {
+    displayText.textContent = 1;
+})  
+twoButton.addEventListener("click", () => {
+    displayText.textContent = 2;
+})  
+threeButton.addEventListener("click", () => {
+    displayText.textContent = 3;
+})  
+fourButton.addEventListener("click", () => {
+    displayText.textContent = 4;
+})  
+fiveButton.addEventListener("click", () => {
+    displayText.textContent = 5;
+})  
+sixButton.addEventListener("click", () => {
+    displayText.textContent = 6;
+})  
+sevenButton.addEventListener("click", () => {
+    displayText.textContent = 7;
+})  
+eightButton.addEventListener("click", () => {
+    displayText.textContent = 8;
+})  
+nineButton.addEventListener("click", () => {
+    displayText.textContent = 9;
+})  
+

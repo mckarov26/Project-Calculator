ed9207ff877416a161eac74c81fd1357bf057349
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-01-28 21:47:19 +1300

    Refactor button references to querySelectorAll and add event listeners
    - Replaced individual button queries with querySelectorAll for easier iteration with forEach.
    - Added event listeners to update the display and store values for num1, num2, and operator.
    - Implemented event listener for performing only ONE OPERATION calculation and displaying result.
    - Added event listener functionality for resetting the display.

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 const displayText = document.querySelector(".display-text");
-const clearBtn = document.querySelector("#clear-btn")
+const clearButton = document.querySelector("#clear-btn")
 const percentButton = document.querySelector("#percent-btn")
 const backButton = document.querySelector("#back-btn")
-
-// Operator Reference
-const divisionButton = document.querySelector("#division-btn");
-const multiplyButton = document.querySelector("#multiply-btn");
-const subtractButton = document.querySelector("#subtract-btn");
-const additionButton = document.querySelector("#addition-btn");
 
 const equalButton = document.querySelector(".equal-btn");
 const decimalButton = document.querySelector(".decimal-btn");
 
-//Number Reference
+// Operator Reference
+const operatorButtons = document.querySelectorAll(".operator-btn");
 
-const zeroButton = document.querySelector("#zero-btn");
-const oneButton = document.querySelector("#one-btn");
-const twoButton = document.querySelector("#two-btn");
-const threeButton = document.querySelector("#three-btn");
-const fourButton = document.querySelector("#four-btn");
-const fiveButton = document.querySelector("#five-btn");
-const sixButton = document.querySelector("#six-btn");
-const sevenButton = document.querySelector("#seven-btn");
-const eightButton = document.querySelector("#eight-btn");
-const nineButton = document.querySelector("#nine-btn");
+// const divisionButton = document.querySelector("#division-btn");
+// const multiplyButton = document.querySelector("#multiply-btn");
+// const subtractButton = document.querySelector("#subtract-btn");
+// const additionButton = document.querySelector("#addition-btn");
+
+    
+// Number Reference
+
+const numButtons = document.querySelectorAll(".num-btns");
+
+// const zeroButton = document.querySelector("#zero-btn");
+// const oneButton = document.querySelector("#one-btn");
+// const twoButton = document.querySelector("#two-btn");
+// const threeButton = document.querySelector("#three-btn");
+// const fourButton = document.querySelector("#four-btn");
+// const fiveButton = document.querySelector("#five-btn");
+// const sixButton = document.querySelector("#six-btn");
+// const sevenButton = document.querySelector("#seven-btn");
+// const eightButton = document.querySelector("#eight-btn");
+// const nineButton = document.querySelector("#nine-btn");
 
 
-//Variables to store the operands and operator of a Math operations.
+
+
+
+
+//Variables to store the operands and operator of a Math 
 let num1 = "";
 let num2 = "";
 let operator = "";
+
 
 
 // Functions for basic Math operations.
@@ -70,32 +80,62 @@ function operate(num1, num2, operator) {
     }
 }
 
-//Attached Eventlisteners to the number buttons variables.
-oneButton.addEventListener("click", () => {
-    displayText.textContent = 1;
-})  
-twoButton.addEventListener("click", () => {
-    displayText.textContent = 2;
-})  
-threeButton.addEventListener("click", () => {
-    displayText.textContent = 3;
-})  
-fourButton.addEventListener("click", () => {
-    displayText.textContent = 4;
-})  
-fiveButton.addEventListener("click", () => {
-    displayText.textContent = 5;
-})  
-sixButton.addEventListener("click", () => {
-    displayText.textContent = 6;
-})  
-sevenButton.addEventListener("click", () => {
-    displayText.textContent = 7;
-})  
-eightButton.addEventListener("click", () => {
-    displayText.textContent = 8;
-})  
-nineButton.addEventListener("click", () => {
-    displayText.textContent = 9;
-})  
+
+//Event Listener for updating display and storing values to num1 and num2 Variable.
+numButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        if (num1 === "" || operator === "") {
+            num1 += button.textContent;
+            
+        } else if (num1 !== "" && operator !== ""  ) {
+            num2 += button.textContent;
+        }
+
+        
+       displayText.textContent += button.textContent;
+
+       
+    })
+})
+
+//Event Listener for updating display and storing values to operator Variable.
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        if (operator === "") {
+            operator = button.textContent;
+
+            displayText.textContent += button.textContent;
+
+        } else if (operator !== "") {
+            button.disabled = true;
+        }                      
+        
+    })
+})
+
+//Event Listener for operating basic Math and showing the result to the display container.
+equalButton.addEventListener("click", () => {
+    displayText.textContent = "";
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    let result = operate(num1, num2, operator);
+    displayText.textContent = result.toString();
+
+  
+})
+
+
+
+//Event Listener for clearing the display container.
+clearButton.addEventListener("click", () => {
+    displayText.textContent = "";
+})
+
+
+
+
+
+
 

3d886ac03980407321903d6c84c3f4bf7fb6457b
Mckarov26 <[166902834+mckarov26@users.noreply.github.com]>
2025-02-02 15:16:41 +1300

    Completed Project: Calculator (Feb 2, 2025)
    - Please refer to comments for review.
    - I will remake this project again for improvement and to make it clean code.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Project-Calculator
 
-01/25/2025
+01/25/2025 (January 25, 2025)
 Hi there! This is my second attempt at building the Project: Calculator.
 
 Wish me Luck!
+
+
+February  3, 2025
+I have learned a lot while doing this project! I have fixed too many bugs!
+I'm not sure if there still a bug, but yes, I think I finish this now! I think so...
+I did not commit too much on this project, I just comment every code for me to remember.
+
+Note: Some selectors, I didn't used and I decided not to erase them for now.
+I might used them in the future.
+
+I will try to code this project again in the future, those comments will help me.
+And next time, I will try to commit more, make it better and make it a clean code,
+
+My Apologies for the messy code!
+
+This project is very hard for my current skills that i have now. But I persevered, and completed this project.
+
+If you see any bugs or have any suggestions for improvement, please let me know! Thanks!
+
+
--- script.js
+++ script.js
@@ -31,16 +31,16 @@ const numButtons = document.querySelectorAll(".num-btns");
 // const nineButton = document.querySelector("#nine-btn");
 
 
-
-
-
-
 //Variables to store the operands and operator of a Math 
 let num1 = "";
 let num2 = "";
 let operator = "";
 
 
+// This variables are very important!
+// This is to control enabling/disabling of the % button through out the operation!
+let numbutton = false;
+let optrbutton = false;
 
 // Functions for basic Math operations.
 function add(a, b) {
@@ -54,11 +54,22 @@ function multiply(a, b) {
 }
 function divide(a, b) {
     if(b === 0) {
-        return "Cannot be divided by 0"
+
+
+        disableButton(true);
+        
+        // This is to disable all buttons except clear to reset.
+        clearButton.style["background-color"] = "#10B981";
+        clearButton.style["color"] = "white";    
+       
+        return "Cannot be divided by 0"       
     };
 
     return a / b;
 }
+
+    
+
 
 // Functions  that invokes basic Math operations 
 function operate(num1, num2, operator) {
@@ -67,7 +78,7 @@ function operate(num1, num2, operator) {
         case "+":
             return add(num1, num2);
      
-        case "−":
+        case "-":
             return subtract(num1, num2);
      
         case "×":
@@ -75,21 +86,98 @@ function operate(num1, num2, operator) {
      
         case "÷":
             return divide(num1, num2);
-        default:
-            return "Invalid Operator";
+
+        case "%":
+            return percent(num1, num2);
+     }
+}
+
+// This Function is to disable/enable all buttons except clear and equal to reset
+// This function will be used in equalButton and percentButton event listener
+
+function disableButtonExceptEqual(boolean) {
+    percentButton.disabled = boolean;
+        
+        numButtons.forEach(button => {
+            button.disabled = boolean;
+        })
+        operatorButtons.forEach(button => {
+            button.disabled = boolean;
+        })
+}
+
+// This Function is to disable/enable all buttons except clear to reset.
+// This function will be used in clearButton event listener and divide function.
+function disableButton(boolean) {
+    equalButton.disabled = boolean;
+    percentButton.disabled = boolean;
+    backButton.disabled = boolean;
+
+    numButtons.forEach(button => {
+        button.disabled = boolean;
+    })
+    operatorButtons.forEach(button => {
+        button.disabled = boolean;
+    })
+}
+
+
+
+// function for changing color when "mouseover" hover in clear button.
+function hoverInColor() {
+    clearButton.style.backgroundColor = "hsl(160, 84%, 50%)";
+    clearButton.style.color = "white";
+}
+
+
+// function for changing color when "mouseout" hover in clear button
+function hoverOutColor() {
+    clearButton.style.backgroundColor = "#10B981";
+    clearButton.style.color = "white";
+}
+// function to operate percentage math operations.
+function divideHundred(operator, percentageResult, num1) {
+    switch(operator) {
+        case "+":
+            return percentageResult + num1;
+        case "-":
+            return percentageResult - num1;
+        case "×":
+            return percentageResult * num1;
+        case "÷":
+            return percentageResult / num1;
     }
 }
 
+// This Event listener is to ensure the decimal button is click only once on each operands!
+decimalButton.addEventListener("click", () => {
+
+        if (num1 === "" || num1 !== "") {
+            decimalButton.disabled = true;
+        } else if (num1 !== "" && operator !== "") {
+            decimalButton.disabled = true;
+        }
+        
+   
+    
+})
 
 //Event Listener for updating display and storing values to num1 and num2 Variable.
 numButtons.forEach(button => {
     button.addEventListener("click", () => {
 
-        if (num1 === "" || operator === "") {
+        if (num1 === "" || num1 === "-" || operator === "") {
             num1 += button.textContent;
-            
+
+            operatorButtons.forEach(button => {
+                button.disabled = false;
+            })
+           
         } else if (num1 !== "" && operator !== ""  ) {
             num2 += button.textContent;
+            numbutton = true;
+            optrbutton = true;
+            
         }
 
         
@@ -103,13 +191,43 @@ numButtons.forEach(button => {
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
 
-        if (operator === "") {
-            operator = button.textContent;
+        // This is to ensure that decimal button is enabled after operator buttons is clicked!
+        decimalButton.disabled = false;
 
+
+        numbutton = false;
+        optrbutton = false;
+
+
+        // This will ensure that user can used negative sign ONLY before num1
+        // disable other operator buttons before num1
+        if (button.textContent !== "-" && num1 === "") {
+            button.disabled = true;
+            
+
+        }
+        // // And to disable other buttons when negative button is click
+        else if (button.textContent === "-" && num1 === "") {
+
+           operatorButtons.forEach(button => {
+            button.disabled = true;
+           })
+
+            
+            num1 +=  button.textContent;
+            displayText.textContent += button.textContent;
+
+        } else if (operator === "") {
+
+            operator = button.textContent;
+            optrbutton = true;
             displayText.textContent += button.textContent;
 
         } else if (operator !== "") {
             button.disabled = true;
+            
+           
+
         }                      
         
     })
@@ -117,22 +235,187 @@ operatorButtons.forEach(button => {
 
 //Event Listener for operating basic Math and showing the result to the display container.
 equalButton.addEventListener("click", () => {
-    displayText.textContent = "";
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    let result = operate(num1, num2, operator);
-    displayText.textContent = result.toString();
 
-  
+    numbutton = false;
+    optrbutton = false;
+
+    disableButtonExceptEqual(false)
+    // This is to give result if only num1 exist with %
+    if (num1.toString().includes("%")) {
+        let result1 = parseFloat(num1) / 100;
+        displayText.textContent = result1.toString();
+
+
+
+        operator = "";
+        num2 = "";
+        num1 = parseFloat(result1);
+
+   // This is to give result if num1 exist and num2 exist with %.
+
+    } else if (num1 !== "" && operator !== "" && num2.toString().includes("%")) {
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
+        let percentageResult = (num1 * num2) / 100 ;
+        let result = divideHundred(operator, percentageResult, num1)
+        operator = "";
+        num2 = "";
+        num1 = parseFloat(result);
+
+
+
+        displayText.textContent = result.toString();
+
+       
+
+    
+    } 
+   
+
+
+
+    else if (num1 !== "" && operator !== "" && num2 !== "") {
+        displayText.textContent = "";
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
+    
+        let result2 = operate(num1, num2, operator);
+        
+        displayText.textContent = result2.toString();
+        
+
+        operator = "";
+        num2 = "";
+        num1 = parseFloat(result2);
+
+
+        if (displayText.textContent === "Cannot be divided by 0") {
+            clearButton.addEventListener("mouseover", hoverInColor);
+            clearButton.addEventListener("mouseout", hoverOutColor);
+        }
+
+   
+
+        
+    }
 })
 
+ 
 
 
 //Event Listener for clearing the display container.
 clearButton.addEventListener("click", () => {
+
+    numbutton = false;
+    optrbutton = false;
+
     displayText.textContent = "";
+
+    num1 = "";
+    num2 = "";
+    operator = "";
+
+    disableButton(false);
+
+
+    // This is to ensure that it will revert to its default color when click
+    clearButton.style["background-color"] = "";
+    clearButton.style["color"] = "";
+
+
+    // This is to ensure that this event listeners won't apply in the future after click the clear button
+    clearButton.removeEventListener("mouseover", hoverInColor);
+    clearButton.removeEventListener("mouseout", hoverOutColor);
+  
 })
 
+// Event Listener for clicking the Percent Button
+// This is to disable some buttons being click after clicking the percent
+// This is to avoid unexpected math operation. 
+
+percentButton.addEventListener("click", () => {
+ 
+        //This is to make sure that percent button won't be click if negative signs shows without number.
+    if(num1 === "-") {
+        numbutton = false;
+        optrbutton = false;    
+    }
+
+    else if ( numbutton && optrbutton)  {
+        num2 += percentButton.textContent;
+        displayText.textContent += percentButton.textContent;
+        disableButtonExceptEqual(true);
+    
+    } 
+
+    else if ( num1 !== "" && operator === "" && num2 === "") {
+        num1 += percentButton.textContent;
+        displayText.textContent += percentButton.textContent;
+        
+        disableButtonExceptEqual(true);
+        
+
+    }
+    
+
+})
+
+
+
+// Event Listener for click backButton
+backButton.addEventListener("click", () => {
+    disableButtonExceptEqual(false);
+
+
+    // This is to enable/disable decimal button to prevent multiple decimals in num1 or num2.
+    if (num2.includes(".")) {
+        decimalButton.disabled = false;
+    } else if (num1.includes(".") && operator !== "" ) {
+        decimalButton.disabled = true;
+    } else if (num1 !== "") {
+        decimalButton.disableButton = false;
+    }
+    
+    // numbutton = false;
+    // optrbutton = false;
+
+    // This is enable/disable percent button to prevent multiple percent in num1 or num2.
+    if (num1 !== "" && operator !== "" && num2 !== "") {
+        numbutton = true;
+        optrbutton = true;
+    }
+
+    // This code below will help me convert number values to string.
+    //This will be used for checking if there is a % string inside the displaytext.
+    //And to make sure I can control the limit of used of "%" inside num1 and num2
+    num1 = num1.toString();
+    num2 = num2.toString();
+
+    if (num2.includes("%")) {
+        num2 = num2.replace("%", "");
+        displayText.textContent = `${num1} ${operator} ${num2}`;
+        
+
+    } else if (num1.includes("%", "")) {
+        num1 = num1.replace("%", "")
+        displayText.textContent = `${num1}`;
+        
+
+    } else if (num2 !== "") {
+        num2 = "";
+        displayText.textContent = `${num1} ${operator}`;
+        numbutton = false;
+        optrbutton = false;
+    } else if (operator !== "") {
+        operator = "";
+        displayText.textContent = `${num1}`;
+    } else if (num1 !== "") {
+        num1 = "";
+        displayText.textContent = "";
+    }
+})
 
 
 
